cea240d0e789987a57de1a800f0286fb112e0ac5
SandipPandhare <93873281+SandipPandhare@users.noreply.github.com>
2023-01-04 22:17:33 +0530

    Add files via upload

---
+++ program11.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void display(int iRow,int iCol)
+{
+    for(int i=1;i<=iRow;i++)
+    {
+        for(int j=1;j<=iCol;j++)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+    }
+}
+int main()
+{
+    int iValue1=0,iValue2=0;
+    printf("Enter no of rows : ");
+    scanf("%d",&iValue1);
+    printf("Enter no of column : ");
+    scanf("%d",&iValue2);
+
+    display(iValue1,iValue2);
+
+    return 0;
+}
